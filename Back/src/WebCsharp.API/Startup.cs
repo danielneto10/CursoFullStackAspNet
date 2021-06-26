5286174b4fb6742073b41d025fcc7d11aa498842
danielneto10 <daniel.fluz@hotmail.com>
2021-06-26 20:16:13 -0300

    Adição do Angular

--- Back/src/WebCsharp.API/Startup.cs
+++ Back/src/WebCsharp.API/Startup.cs
@@ -32,6 +32,7 @@ namespace WebCsharp.API
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebCsharp.API", Version = "v1" });
@@ -53,6 +54,10 @@ namespace WebCsharp.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
