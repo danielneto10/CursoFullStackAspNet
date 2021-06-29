3e88b53b472938b0fe02f489e2fd0ba6004394e3
danielneto10 <daniel.fluz@hotmail.com>
2021-06-29 19:16:28 -0300

    Adição da navbar e do grid de eventos

--- Front/WebCsharp-APP/src/app/eventos/eventos.component.ts
+++ Front/WebCsharp-APP/src/app/eventos/eventos.component.ts
@@ -8,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 100;
+  marginImg: number = 2;
+  exibirImg: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    console.log(this.filtroLista);
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,10 +40,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public alterarImg(): void {
+    this.exibirImg = !this.exibirImg;
+  }
+
   public getEventos(): void {
 
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
   }
