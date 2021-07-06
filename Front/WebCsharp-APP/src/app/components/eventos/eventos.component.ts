4e5ce544a25d8b75f92ffbb59b474a07f03cdbf6
danielneto10 <daniel.fluz@hotmail.com>
2021-07-06 19:28:48 -0300

    Organização/Adição - Rotas, Alertas e +

---
+++ Front/WebCsharp-APP/src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+
+import { Evento } from '../../models/Evento';
+import { EventoService } from '../../services/evento.service';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+  modalRef = {} as BsModalRef;
+
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
+
+  public widthImg: number = 100;
+  public marginImg: number = 2;
+  public exibirImg: boolean = true;
+  private filtroListado: string = '';
+
+  public get filtroLista() {
+    return this.filtroListado
+  }
+
+  public set filtroLista(value: string) {
+    this.filtroListado = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
+
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+    ) { }
+
+  public ngOnInit() {
+    this.spinner.show();
+    this.getEventos();
+  }
+
+  public alterarImg(): void {
+    this.exibirImg = !this.exibirImg;
+  }
+
+  public getEventos(): void {
+
+    this.eventoService.getEventos().subscribe({
+      next: (eventos: Evento[]) => {
+        this.eventos = eventos;
+        this.eventosFiltrados = this.eventos;
+      },
+      error: error => {
+        this.spinner.hide();
+        this.toastr.error('Erro ao carregar os Eventos', 'Erro!');
+      },
+      complete: () => this.spinner.hide()
+    });
+  }
+
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.toastr.success('O Evento foi deletado com sucesso', 'Deletado');
+    this.modalRef.hide();
+  }
+
+  decline(): void {
+    this.modalRef.hide();
+  }
+}
